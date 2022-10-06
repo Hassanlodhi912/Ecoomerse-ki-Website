32c6930c69c4117bcd89a3b25128ba628e84b947
hassan <muhammadhassanlodhi123@gmail.com>
2022-10-06 17:07:26 +0500

    first commit

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "./components/HeroSection";
+
+const About = () => {
+  const data = {
+    name: "Thapa Ecommerce",
+  };
+
+  return <HeroSection myData={data} />;
+};
+
+export default About;
