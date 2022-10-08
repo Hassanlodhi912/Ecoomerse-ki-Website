f958307d7e7232f98c23d0dd0aaee76371587b94
hassan <muhammadhassanlodhi123@gmail.com>
2022-10-08 21:24:12 +0500

    toggle added just color  settingRemaining

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,18 +1,21 @@
+import { useState } from "react"
 import { MdDarkMode, MdLightMode } from "react-icons/md"
 import { useDispatch, useSelector } from "react-redux"
 import { setToggle } from "../store/action/Index"
 const Toggle = () => {
+   const darkMode =useSelector((state)=>state.ToggleReducer.darkMode)
     const dispatch = useDispatch();
     const handleClick = () => {
         dispatch(setToggle())
     }
+
     return (
-        <div >
+        <div onClick={handleClick} >
 
-            <h2 onClick={handleClick} >
-
-                <MdLightMode  />
-                <MdDarkMode  />
+            <h2  >
+                {
+                    darkMode  ? ( <MdLightMode  /> ) : (       <MdDarkMode />   )
+                }
 
             </h2>
         </div>
